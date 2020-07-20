db5a82c3cf97120f8be6027ac968eec9e835662b
岩井　將輝 <>
2020-07-20 13:41:39 +0900

    kansei

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root 'home#top'
+  get 'home/about'
+  resources :users, only: [:show, :index, :edit, :update] do
+  	resource :relationships, only: [:create, :destroy]
+    get :follows, on: :member # 追加
+    get :followers, on: :member # 追加
+  end
+  
+  resources :books, except: [:new] do
+    resources :book_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+  end
+end
